3b59dd5dd74701a894f1455b5f8f4cf9a335f763
SAKSHI SARAIYA <sakshisaraiya@gmail.com>
2025-07-05 11:10:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -62,4 +62,4 @@ with right_col:
 # --- Footer ---
 st.markdown("---")
 st.markdown("<div style='text-align:center;'>🔒 Secure | ⚡ Fast | 🎯 Accurate</div>", unsafe_allow_html=True)
-st.markdown("<div style='text-align:center; font-size:12px;'>Built with ❤️ by Sakshi Saraiya & chirag Thakkar/div>", unsafe_allow_html=True)
+st.markdown("<div style='text-align:center; font-size:12px;'>Built with ❤️ by Sakshi Saraiya & Chirag Thakkar/div>", unsafe_allow_html=True)
